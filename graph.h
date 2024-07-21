badf4c3b57c9f8bd21489ed2426a6d99d8ea7c2a
batramahek <mahek.batra@outlook.com>
2024-07-21 19:36:25 -0400

    fixing errors

--- graph.h
+++ graph.h
@@ -1,3 +1,4 @@
+#pragma once
 //
 // Created by Mahek Batra on 2024-07-15.
 //
@@ -6,25 +7,27 @@
 #include <queue>
 #include <string>
 
+
+
 #ifndef COP3530_PROJECT_3_GRAPH_H
 #define COP3530_PROJECT_3_GRAPH_H
 
-using namespace std; 
+using namespace std;
 
 class AdjacencyList
 {
 private:
-	unordered_multimap<string, vector<pair<string, double>>> graph;
-	unordered_multimap<string, pair<double, double>> nodeLocations;
+	unordered_multimap<int, vector<pair<int, double>>> graph;
+	unordered_multimap<int, pair<double, double>> nodeLocations;
 
 public:
 	void insertNode(int nodeID, double latitude, double longitude)
 	{
-		nodeLocations[nodeID] = { latitude, longitude };
+		nodeLocations.insert({ nodeID, {latitude, longitude} });
 	}
 	void insertEdge(int fromID, int toID, double weight)
 	{
-		graph[fromID].push_back(make_pair <toID, weight>);
+		graph.insert({ fromID, {{toID, weight}} });
 	}
 	//code referenced from Lecture Slides - Mod 8a - Graphs Terminology and Implementation -- slide 64
 	vector<int> bfs(int startID, int endID)
@@ -37,7 +40,7 @@ public:
 		//insert and push startID into visited and queue
 		visited.insert(startID);
 		queue.push(startID);
-		
+
 		//set node previous to start id as -1
 		previous[startID] = -1;
 
@@ -55,7 +58,7 @@ public:
 				}
 
 				//reverse order of path
-				reverse(pathTrace.begin(), path.end());
+				reverse(pathTrace.begin(), pathTrace.end());
 				return pathTrace;
 			}
 
@@ -63,7 +66,7 @@ public:
 
 			//check if element is in the visited set, if its not, add it to vector and queue
 			vector<int> neighbours = graph[u];
-			for (int v : neighbours)
+			for (const auto& v : neighbours)
 			{
 				if (visited.count(v) == 0)
 				{
@@ -78,7 +81,7 @@ public:
 		}
 	}
 	vector<int> dijkstras(int startID, int endID);
-	unordered_multimap<int, pair<int, double>>& getAdjacencyList()
+	unordered_multimap<int, vector<pair<int, double>>>& getAdjacencyList()
 	{
 		return graph;
 	}
--- main.cpp
+++ main.cpp
@@ -11,68 +11,68 @@
 //Earth Radius in Kilometers found online
 const double EarthR = 6378.0;
 
-double Formula(double Latitude_1, double Longitude_1, double Latitude_2, double Longitude_2){
+double Formula(double Latitude_1, double Longitude_1, double Latitude_2, double Longitude_2) {
     double Longitude = (Latitude_2 - Latitude_1) * pi / 180.0;
     double Latitude = (Longitude_2 - Longitude_1) * pi / 180.0;
 
     Latitude_1 = Latitude_1 * pi / 180.0;
     Latitude_2 = Latitude_2 * pi / 180.0;
 
-    double Result = sin(Latitude/2) * sin(Latitude/2) + cos(Latitude_1) * cos(Latitude_2) * sin(Longitude/2) * sin(Longitude/2); 
-    double Result_1 = 2 * atan2(sqrt(Result), sqrt(1-Result));
+    double Result = sin(Latitude / 2) * sin(Latitude / 2) + cos(Latitude_1) * cos(Latitude_2) * sin(Longitude / 2) * sin(Longitude / 2);
+    double Result_1 = 2 * atan2(sqrt(Result), sqrt(1 - Result));
     double final = EarthR * Result_1;
     return final;
 }
 
-class Drawer : public osmium::handler::Handler{
-    public:
-        AdjacencyList& graph;
-        int Node_Total = 0;
+class Drawer : public osmium::handler::Handler {
+public:
+    AdjacencyList& graph;
+    int Node_Total = 0;
 
-        const int Node_Max = 100000;
-        Drawer(AdjancencyList& list) : graph(list) {}
+    const int Node_Max = 100000;
+    Drawer(AdjacencyList& list) : graph(list) {}
 
-        void nodes(const osmium::Node& nodes){
-            if (Node_Total < Node_Max) {
-                graph.insertNode(nodes.id(), nodes.location.Lat(), nodes.location.Long());
-                Node_Total++;
+    void nodes(const osmium::Node& nodes) {
+        if (Node_Total < Node_Max) {
+            graph.insertNode(nodes.id(), nodes.location.Lat(), nodes.location.Long());
+            Node_Total++;
+        }
+    }
+
+    void Direction(const osmium::Way& Direct) {
+        const auto& Direction_of_Node = Direct.nodes();
+        for (size_t x = 1; x, Direction_of_Node.size(); x++) {
+            auto Node_1 = Direction_of_Node[x - 1].ref();
+            auto Node_2 = Direction_of_Node[x].ref();
+            if (graph.getNodeLoc().count(Node_1) && graph.getNodeLoc().count(Node_2))
+            {
+                double Latitude_1 = graph.getNodeLoc().at(Node_1).first;
+                double Longitude_1 = graph.getNodeLoc().at(Node_1).second;
+                double Latitude_2 = graph.getNodeLoc().at(Node_2).first;
+                double Longitude_2 = graph.getNodeLoc().at(Node_2).second;
+                double Dist = Formula(Latitude_1, Longitude_1, Latitude_2, Longitude_2);
+                graph.insertEdge(Node_1, Node_2, Dist);
             }
         }
-
-        void Direction (const osmium::Way& Direct){
-            const auto& Direction_of_Node = Direct.nodes();
-            for (size_t x = 1; x, Direction_of_Node.size(); x++){
-                auto Node_1 = Direction_of_Node[x-1].ref();
-                auto Node_2 = Direction_of_Node[x].ref();
-                if (graph.getNodeLoc().count(Node_1)&&graph.getNodeLoc().count(Node_2))
-                {
-                    double Latitude_1 = graph.getNodeLoc().at(Node_1).first;
-                    double Longitude_1 = graph.getNodeLoc().at(Node_1).second;
-                    double Latitude_2 = graph.getNodeLoc().at(Node_2).first;
-                    double Longitude_2 = graph.getNodeLoc().at(Node_2).second;
-                    double Dist = Formula(Latitude_1, Longitude_1, Latitude_2, Longitude_2);
-                    graph.insertEdge(Node_1, Node_2, Dist);
-                }
-            }
-        }
+    }
 };
 
-void OSMDATA(const string& name, AdjacencyList& graph){
+void OSMDATA(const string& name, AdjacencyList& graph) {
     osmium::io::Reader reader(name);
     Drawer handler(graph);
     osmium::apply(reader, handler);
     reader.close();
 }
 
-void GraphVisual(sf::RenderWindow& window, const AdjacencyList& graph){
+void GraphVisual(sf::RenderWindow& window, const AdjacencyList& graph) {
     sf::CircleShape nodeShape(2);
     nodeShape.setFillColor(sf::Color::Blue);
     sf::VertexArray edges(sf::Lines);
-    for (const auto& couple : graph.getAdjList()) {
+    for (const auto& couple : graph.getAdjacencyList()) {
         int Node_1 = couple.first;
         double Latitude_1 = graph.getNodeLoc().at(Node_1).first;
         double Longitude_1 = graph.getNodeLoc().at(Node_1).second;
-        for(const auto& neighbors : couple.second) {
+        for (const auto& neighbors : couple.second) {
             int Node_2 = neighbors.first;
             double Latitude_2 = graph.getNodeLoc().at(Node_2).first;
             double Longitude_2 = graph.getNodeLoc().at(Node_2).second;
@@ -83,7 +83,7 @@ void GraphVisual(sf::RenderWindow& window, const AdjacencyList& graph){
         }
     }
     window.draw(edges);
-    for(const auto& node : graph.getNodeLoc()){
+    for (const auto& node : graph.getNodeLoc()) {
         double Latitude = node.second.first;
         double Longitude = node.second.second;
         nodeShape.setPosition(Longitude * 1000, Latitude * 1000);
@@ -92,24 +92,24 @@ void GraphVisual(sf::RenderWindow& window, const AdjacencyList& graph){
 }
 
 
-int main (){
+int main() {
     string osm_filename = "florida-latest.osm.pbf";
     AdjacencyList graph;
     OSMDATA(osm_filename, graph);
 
     sf::RenderWindow window(sf::VideoMode(800, 600), "Project 3");
-    
+
     while (window.isOpen())
     {
         sf::Event event;
         while (window.pollEvent(event))
         {
-            if(event.type == sf::Event::Closed)
+            if (event.type == sf::Event::Closed)
                 window.close();
         }
         window.clear();
         GraphVisual(window, graph);
-        window.dislplay();
+        window.display();
     }
     return 0;
 }
