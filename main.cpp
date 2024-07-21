9a3f9d8ae30fd670476b9c3603daffbdc5f0744b
ynpatel910 <yn.patel@ufl.edu>
2024-07-21 13:44:55 -0400

    SFML starting code for Main file

--- main.cpp
+++ main.cpp
@@ -1,1 +1,21 @@
-int main ();
+#include <SFML/Graphics.hpp>
+
+int main (){
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Project 3");
+    
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEven(event))
+        {
+            if(event.type == sf::Event::Closed)
+                window.close();
+        }
+        window.clear();
+        /* This will show the actual graph in the window, set up later
+        window.draw(example);
+        */
+        window.dislplay();
+    }
+    return 0;
+}
